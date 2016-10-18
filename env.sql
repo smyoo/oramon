791c3d25498135e9500b2c6dff66a458bac6166f
smyoo <smyoo0316@gmail.com>
2016-10-18 13:00:01 +0900

    add file env.sql
    add file env.sql

---
+++ env.sql
@@ -0,0 +1,9 @@
+set lines 300
+set pages 20
+set underline =
+set verify off
+col owner for a15
+col table_name for a35
+col tablespace_name for a35
+col comments for a80
+col size_mb for 999,999,999,999

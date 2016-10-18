47eb6f1fdb6a961597b4d54c3bcb275a41aab42d
smyoo <smyoo0316@gmail.com>
2016-10-18 13:01:42 +0900

    add file tab.sql
    add file tab.sql

---
+++ tab.sql
@@ -0,0 +1,20 @@
+/***********************************************
+Query Table Information
+************************************************/
+
+@env
+
+define _t_name = &_table_name
+
+select a.TABLE_NAME, a.OWNER, a.TABLESPACE_NAME, a.IOT_TYPE, a.LOGGING, a.DEGREE, a.PARTITIONED, a.COMPRESSION, seg.bytes/1024/1024 as size_mb, tc.comments
+from dba_tables a, dba_tab_comments tc,
+     (select owner, segment_name, sum(bytes) as bytes
+      from dba_segments
+      where segment_name like '%'||upper('&_t_name')||'%' and segment_type like 'TABLE%'
+      group by owner, segment_name
+      ) seg
+where a.owner=tc.owner(+) and a.table_name=tc.table_name(+)
+and a.owner=seg.owner(+) and a.table_name=seg.segment_name(+)
+and a.table_name like '%'||upper('&_t_name')||'%'
+order by a.TABLE_NAME, a.OWNER
+/
